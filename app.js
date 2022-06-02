f41b243a905d15e4218483c8bc508add4362caaa
a0ne <mako666777@gmail.com>
2022-06-02 23:05:48 +0300

    initial

--- app.js
+++ app.js
@@ -59,12 +59,13 @@ app.use('*', () => {
 app.use(errors());
 
 app.use((err, req, res) => {
-  if (err.statusCode) {
-    res.status(err.statusCode).send({ message: err.message });
-    return;
-  }
-
-  res.status(500).send({ message: err.message });
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 });
-
 app.listen(PORT);
