e544668621aa2166cd63bf4616bda8717ec1e4d5
a0ne <mako666777@gmail.com>
2022-06-02 13:28:05 +0300

    initial

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.post(
     body: Joi.object().keys({
       email: Joi.string().required(),
       password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string(),
     }),
   }),
   createUser
